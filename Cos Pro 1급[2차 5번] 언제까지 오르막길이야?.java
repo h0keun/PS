332e3791fd0e443a02759e64ca1f056d176c3bb0
HoKeun.LEE <dlghrms95@gmail.com>
2021-08-03 11:44:42 +0900

    2021-07-31

--- Cos Pro 1급[2차 5번] 언제까지 오르막길이야?.java
+++ Cos Pro 1급[2차 5번] 언제까지 오르막길이야?.java
@@ -24,4 +24,15 @@ class Main {
       answer = Collections.max(list);
       return answer;
     }
+	
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Main sol = new Main();
+        int[] arr = {3, 1, 2, 4, 5, 1, 2, 2, 3, 4};
+        int ret = sol.solution(arr);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
 
