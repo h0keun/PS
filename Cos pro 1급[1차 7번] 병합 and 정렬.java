b7cd9ce6d48785234a6d7a0e6beb267ac12f0af7
HoKeun.LEE <dlghrms95@gmail.com>
2021-08-03 11:41:31 +0900

    2021-07-31

--- Cos pro 1급[1차 7번] 병합 and 정렬.java
+++ Cos pro 1급[1차 7번] 병합 and 정렬.java
@@ -19,3 +19,14 @@ class Main {
             answer[answer_idx++] = arrB[arrB_idx++];
         return answer;
     }
+          // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Main sol = new Main();
+        int[] arrA = {-2, 3, 5, 9};
+        int[] arrB = {0, 1, 5};
+        int[] ret = sol.solution(arrA, arrB);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
+    }
+}
